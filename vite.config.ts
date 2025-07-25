feae954570695b8742a881369dc53cce604cb812
CarmenJuana <carmenjuanamunozamador09@gmail.com>
2025-07-24 23:04:03 -0600

    hellohermosa

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React1.0/',  // <-- Aquí el nombre EXACTO del repo con las barras
   plugins: [react()],
 })
